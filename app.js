0fb674835ecec961fcf4c1c575bfc3c123e7f056
vanlinh00 <82046592+vanlinh00@users.noreply.github.com>
2021-11-09 18:21:02 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,110 @@
+var client = require("./connection.js");
+var express = require('express');
+var app = express();
+const path = require('path'); // Require library to help with filepaths
+
+app.use(express.urlencoded({ extended: false })); // Middleware to recognize strings and arrays in requests
+app.use(express.json()); // Middleware to recognize json in requests
+app.use(express.static("public")); // Must have this or else access to index.js will 404
+app.set("view engine", "ejs");
+app.set("views", "./view");
+
+
+app.get('/hinhanh', function (req, res) {
+  console.log(req.query.id);
+
+  if (req.query.id == "" || req.query.id == undefined) {
+    res.render('test.ejs');
+  }
+  else {
+    client.search({
+      index: 'search-articles',
+      type: 'articles',
+      body: {
+        "query": {
+          "match_phrase": {
+            "description": { query: req.query.id, slop: 100 }
+          }
+        }
+      }
+
+    }).then(function (resp) {
+      console.log("sdfgfdsSuccessful query! Here is the response:", resp.hits.hits[0]._source.description);
+      // res.send(resp.hits.hits[0]._source.Title);
+      res.render('search_img.ejs', { data: resp.hits.hits, key: req.query.id });
+    }, function (err) {
+      console.trace(err.message);
+      res.send(err.message);
+    });
+  }
+});
+
+app.get('/moogle', function (req, res) {
+  // res.render('test.ejs');
+  console.log(req.query.search);
+  /* Query using slop to allow for unexact matches */
+  if (req.query.search == "" || req.query.search == undefined) {
+    res.render('test.ejs');
+  }
+  else {
+    client.search({
+      index: 'search-articles',
+      type: 'articles',
+      body: {
+        "query": {
+          "match_phrase": {
+            "description": { query: req.query.search, slop: 100 }
+          }
+        }
+      }
+
+    }).then(function (resp) {
+
+
+      console.log("sdfgfdsSuccessful query! Here is the response:", resp.hits.hits[0]._source.description);
+
+      // res.send(resp.hits.hits[0]._source.Title);
+      res.render('search_key.ejs', { data: resp.hits.hits, key: req.query.search });
+
+    }, function (err) {
+      console.trace(err.message);
+      res.send(err.message);
+    });
+
+  }
+
+});
+
+// // Route to search for Articles by title
+// app.get('/search-title/:title', function (req, res) {
+//   // Access title like this: req.params.title
+
+//   /* Query using slop to allow for unexact matches */
+//   client.search({
+//     index: 'search-articles',
+//     type: 'articles',
+//     body: {
+//       "query": {
+//         "match_phrase": {
+//           "Title": { query: req.params.title, slop: 100 }
+//         }
+//       }
+//     }
+
+//   }).then(function (resp) {
+
+//     console.log("sdfgfdsSuccessful query! Here is the response:", resp.hits.hits);
+
+//     res.send(resp.hits.hits[0]._source.Title);
+//     // res.render('test.ejs');
+
+//   }, function (err) {
+//     //   console.trace(err.message);
+//     //res.send(err.message);
+//   });
+// });
+
+// Start listening for requests on port 3000
+app.listen(3000, function () {
+  console.log('App listening for requests...');
+});
---
+++ bulkLoad.js
@@ -0,0 +1,45 @@
+var client = require('./connection.js');
+var articlesJson = require("./articles.json");
+var bulkArticlesArray = [];
+
+var makeBulkArray = function(articles, callback){
+  for (var current in articles){
+    bulkArticlesArray.push(
+      { index: {_index: 'search-articles', _type: 'articles', _id: articles[current].ID } },
+      {
+        "description": articles[current]["description"],
+     //   "MetaDescription": articles[current]["MetaDescription"],
+        // "MetaKeywords": articles[current]["MetaKeywords"],
+        // "Categories": articles[current]["Categories"],
+        // "Tags": articles[current]["Tags"],
+        // "Status": articles[current]["Status"]
+      }
+    );
+  }
+  callback(bulkArticlesArray);
+}
+
+var indexArticlesBulk = function(bulkArr, callback) {
+  client.bulk({
+    maxRetries: 5,
+    index: 'search-articles',
+    type: 'articles',
+    body: bulkArr
+  },function(err,resp,status) {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        callback(resp.items);
+      }
+  })
+}
+
+makeBulkArray(articlesJson,function(response){
+  console.log('Bulk Articles: \n');
+  console.log(JSON.stringify(response, null, 2));
+
+  indexArticlesBulk(response,function(response){
+    console.log(response);
+  })
+});
